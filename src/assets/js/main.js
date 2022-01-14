3a375d8429c7b7357d8b9a8ce2e8a99dd33e14b1
Benjamin König <bkoenig.as@gmail.com>
2022-01-14 21:13:07 +0100

    Refactor

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -73,12 +73,18 @@ mobileBtn.addEventListener('click', e => {
 // sticky navigation
 let lastY = window.scrollY;
 const update_sticky_nav_position = e => {
-
     if(lastY < window.scrollY) {
         nav.classList.add('-translate-y-[100px]');
-    } else {
-        nav.classList.remove('-translate-y-[100px]');
 
+	} else {
+        nav.classList.remove('-translate-y-[100px]');
+		if(lastY > 100) {
+			nav.style.background = "#111723";
+
+		} else {
+			nav.style.background = "transparent";
+
+		}
     }
     lastY = window.scrollY;
 }
